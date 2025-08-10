2a53058453a5070e47d69423cb9c7f2d190be604
Prashant Jha <56531447+prashantjha87@users.noreply.github.com>
2025-08-10 11:24:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-// Service Worker for offline functionality
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('daily-gain-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/manifest.json',
+                '/icons/icon-192.png',
+                '/icons/icon-512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
